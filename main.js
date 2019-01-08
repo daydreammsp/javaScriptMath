b058bae4d97ab7710b65739d84c26b83f8cebb1d
Jonathan Kruse <jonathanskruse@gmail.com>
2019-01-07 21:19:40 -0500

    vector methods

---
+++ main.js
@@ -0,0 +1,10 @@
+let v1 = new vector(10, 5),
+    v2 = new vector(3,4);
+
+v1.addTo(v2);
+
+console.log(v1.getX(), v1.getY());
+
+
+
+    
--- vector.js
+++ vector.js
@@ -26,10 +26,37 @@ class vector {
     setLength(length){
         let angle = this.getAngle();
         this._x = Math.cos(angle) * length;
-        this._y = Math.sin(angle) * lendth;
+        this._y = Math.sin(angle) * length;
     }
     getLength(){
         return Math.sqrt(this._x * this._x + this._y * this._y);
     }
-
+    add(v2){
+        return new vector(this._x + v2.getX(), this._y + v2.getY());
+    }
+    subtract(v2){
+        return new vector(this._x - v2.getX(), this._y - v2.getY());
+    }
+    multiply(val){
+        return new vector(this._x * val, this._y * val);
+    }
+    divide(val){
+        return new vector(this._x / val, this._y / val);
+    }
+    addTo(v2){
+        this._x += v2.getX();
+        this._y += v2.getY();
+    }
+    subtractFrom(v2){
+        this._x -= v2.getX();
+        this._y -= v2.getY();
+    }
+    multiply(val){
+        this._x *= val;
+        this._y *= val;
+    }
+    divide(val){
+        this._x /= val;
+        this._y /= val;
+    }
 };
